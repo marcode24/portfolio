f53322f95afcc93f651d51565e8502cd10d316f9
marco cruz <marco24cruz08@gmail.com>
2023-12-05 15:07:52 -0600

    :sparkles: change buttons for send mails

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,7 +2,7 @@ const GITHUB_URL = 'https://github.com/marcode24';
 const LINKEDIN_URL = 'https://www.linkedin.com/in/marco-acg24';
 const TWITTER_URL = 'https://twitter.com/marco_cg24';
 const INSTAGRAM_URL = 'https://www.instagram.com/marcoc_24';
-const EMAIL_URL = 'mailto:marco24cruz08@gmail.com';
+const EMAIL_URL = 'marco24cruz08@gmail.com';
 
 export {
   GITHUB_URL,
